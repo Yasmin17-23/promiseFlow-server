d19b05f8abc05c45dea360bfaf6bcdabb2b3b7cb
Yasmin17-23 <zyasmins1747@gmail.com>
2024-11-25 15:48:09 +0600

    created api for save volunter data

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions = {
+    origin: [
+       'http://localhost:5173',
+       'http://localhost:5174',
+       'https://promiseflow-b4ff2.web.app',
+    ],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
@@ -25,6 +35,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+     
+    const volunteerCollection = client.db('promiseFlow').collection('volunteer');
+
+    app.get('/volunteers', async(req, res) => {
+       const result = await volunteerCollection.find().toArray();
+       res.send(result);
+    })
+
+   //Save a volunteer data in db
+   app.post('/volunteer', async (req, res) => {
+      const volunteerData = req.body;
+      const result = await volunteerCollection.insertOne(volunteerData);
+      res.send(result);
+   })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
