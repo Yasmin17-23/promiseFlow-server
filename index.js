498321f57c9d9871c21ac0414156bd38d9f1907d
Yasmin17-23 <zyasmins1747@gmail.com>
2024-11-29 19:59:40 +0600

    created apis for data by  a specific user

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: [
-       'http://localhost:5173',
-       'http://localhost:5174',
-       'https://promiseflow-b4ff2.web.app',
-    ],
-    credentials: true,
-    optionSuccessStatus: 200,
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://promiseflow-b4ff2.web.app',
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
 }
 
 app.use(cors(corsOptions));
@@ -35,37 +35,51 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-     
-    const organizerCollection = client.db('promiseFlow').collection('organizer');
-    const VolunteerCollection = client.db('promiseFlow').collection('volunteer');
 
-    app.get('/organizers', async(req, res) => {
-       const result = await organizerCollection.find().toArray();
-       res.send(result);
-    })
+    const organizerCollection = client.db('promiseFlow').collection('organizers');
+    const VolunteerCollection = client.db('promiseFlow').collection('volunteers');
 
-
-    //Get a single organizer post from db
-    app.get('/organizers/:id', async (req, res) => {
-       const id = req.params.id;
-       const query = { _id: new ObjectId(id) };
-       const result = await organizerCollection.findOne(query);
-       res.send(result);
+    app.get('/organizers', async (req, res) => {
+      const result = await organizerCollection.find().toArray();
+      res.send(result);
     })
     
-   //Save a oraganizer data in db
-   app.post('/organizer', async (req, res) => {
+    
+   
+
+    //Get a single organizer post from db
+    app.get('/organizer/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await organizerCollection.findOne(query);
+      res.send(result);
+    })
+
+    //Save a volunteer data in db
+    app.post('/volunteer', async (req, res) => {
+      const volunteerData = req.body;
+      const result = await VolunteerCollection.insertOne(volunteerData);
+      res.send(result);
+    })
+
+
+    //Save a oraganizer data in db
+    app.post('/organizer', async (req, res) => {
       const organizerData = req.body;
       const result = await organizerCollection.insertOne(organizerData);
       res.send(result);
-   })
+    })
+    
+     //Get all organizers data from db by a specific user
+     app.get('/organizers/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { 'owner.email': email };
+      const result = await organizerCollection.find(query).toArray();
+      res.send(result);
+    })
 
-  //Save a volunteer data in db
-  app.post('/volunteer', async (req, res) => {
-     const volunteerData = req.body;
-     const result = await VolunteerCollection.insertOne(volunteerData);
-     res.send(result);
-  })
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -79,9 +93,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello From PromiseFlow all Volunteer..')
+  res.send('Hello From PromiseFlow all Volunteer..')
 })
 
 app.listen(port, () => {
-    console.log(`PromiseFlow Server is running on port: ${port}`);
+  console.log(`PromiseFlow Server is running on port: ${port}`);
 })
