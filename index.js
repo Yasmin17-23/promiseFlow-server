f772fe49f95d7f3e754db8925f1303fc3489e205
Yasmin17-23 <zyasmins1747@gmail.com>
2024-12-03 12:25:32 +0600

    created apis for all data to implement search function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion, ObjectId, serialize } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,7 +19,24 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
 
+//Verify jwt middleware
+//const verifyToken = (req, res, next) => {
+//    const token = req.cookies?.token;
+//    if(!token) return res.status(401).send({ message: 'unauthorized access' });
+//     if (token) {
+//      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//           if (err) {
+//           console.log(err);
+//           return res.status(401).send({ message: 'unauthorized access' });
+//            }
+//            console.log(decoded);
+//            req.user = decoded;
+//            next();
+//        } )
+//      }
+//}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hmtao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -39,14 +58,40 @@ async function run() {
     const organizerCollection = client.db('promiseFlow').collection('organizers');
     const VolunteerCollection = client.db('promiseFlow').collection('volunteers');
 
+    //jwt generate
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+         expiresIn: '365d',
+      })
+      res.cookie('token', token, {
+         httpOnly: true,
+         secure: process.env.NODE_ENV === 'production',
+         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      })
+      .send({ success: true })
+    })
+
+    //Clear token on logout
+    app.get('/logout', (req, res) => {
+       res
+       .clearCookie('token', {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          maxAge: 0
+       })
+       .send({ success: true })
+    })
+    
+
+    //Get all oraganizers data from db
     app.get('/organizers', async (req, res) => {
-      const result = await organizerCollection.find().toArray();
+      const result = await organizerCollection.find().sort({ deadline: 1 }).toArray();
       res.send(result);
     })
     
-    
-   
-
+  
     //Get a single organizer post from db
     app.get('/organizer/:id', async (req, res) => {
       const id = req.params.id;
@@ -117,6 +162,16 @@ async function run() {
        const result = await VolunteerCollection.deleteOne(query);
        res.send(result);
     })
+
+   //Get all oraganizers data from db for implement search
+   app.get('/all-organizers', async (req, res) => {
+    const search = req.query.search || '';
+    let query = {
+       postTitle: { $regex: search, $options: 'i' },
+    }
+    const result = await organizerCollection.find(query).sort({ deadline: 1 }).toArray();
+    res.send(result);
+  })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
